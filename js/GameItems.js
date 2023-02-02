9a7639d5e767ea99572dafb9ea9811f2c6a2ff7e
Nikola Tapandzelic <nikola.tapandzelic@brame.io>
2023-02-02 10:28:14 +0100

    refactor GameItems class

--- js/GameItems.js
+++ js/GameItems.js
@@ -3,15 +3,83 @@ export default class GameItems extends Phaser.Physics.Arcade.Group {
     super(world, scene);
 
     this.gameItemsOnScreen = [];
-    this.allGameObjects = [];
+    this.items = [];
   }
 
   initiate() {
-    this.allGameObjects.push(
-      this.create(100, 100, "1din").setScale(0.2).setData({ value: 1 })
+    // this.gameItemsGroup = this.physics.add.group();
+    this.gameItemsOnScreen.push(
+      this.create(200, 220, "1din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 1 })
     );
-    this.allGameObjects.push(
-      this.create(400, 400, "5din").setScale(0.2).setData({ value: 5 })
+    this.gameItemsOnScreen.push(
+      this.create(600, 220, "5din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 5 })
+    );
+    this.items.push(
+      this.create(500, 500, "2din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 2 })
+    );
+    this.items.push(
+      this.create(500, 500, "10din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 10 })
+    );
+    this.items.push(
+      this.create(500, 500, "20din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 20 })
+    );
+
+    this.items.push(
+      this.create(555, 555, "1din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 1 })
+    );
+    this.items.push(
+      this.create(555, 555, "5din")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 5 })
+    );
+    this.items.push(
+      this.create(555, 555, "10din-paper")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 10 })
+    );
+    this.items.push(
+      this.create(555, 555, "20din-paper")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 20 })
+    );
+    this.items.push(
+      this.create(555, 555, "100din-paper")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 100 })
+    );
+    this.items.push(
+      this.create(555, 555, "200din-paper")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 200 })
+    );
+    this.items.push(
+      this.create(555, 555, "500din-paper")
+        .setScale(0.3)
+        .refreshBody()
+        .setData({ value: 500 })
     );
   }
 }
--- js/MainGame.js
+++ js/MainGame.js
@@ -1,3 +1,5 @@
+import GameItems from "./GameItems.js";
+
 export default class MainGame extends Phaser.Scene {
   constructor() {
     super("MainGame");
@@ -46,123 +48,40 @@ export default class MainGame extends Phaser.Scene {
       }
     );
 
-    this.gameItemsGroup = this.physics.add.group();
-    this.gameItemsOnScreen.push(
-      this.gameItemsGroup
-        .create(200, 220, "1din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 1 })
-    );
-    this.gameItemsOnScreen.push(
-      this.gameItemsGroup
-        .create(600, 220, "5din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 5 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(500, 500, "2din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 2 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(500, 500, "10din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 10 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(500, 500, "20din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 20 })
-    );
-
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "1din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 1 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "5din")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 5 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "10din-paper")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 10 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "20din-paper")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 20 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "100din-paper")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 100 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "200din-paper")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 200 })
-    );
-    this.gameItems.push(
-      this.gameItemsGroup
-        .create(555, 555, "500din-paper")
-        .setScale(0.3)
-        .refreshBody()
-        .setData({ value: 500 })
-    );
+    this.gameItems = new GameItems(this.physics.world, this);
+    this.gameItems.initiate();
 
     this.input
-      .setHitArea(this.gameItemsGroup.getChildren())
+      .setHitArea(this.gameItems.getChildren())
       .on("gameobjectup", (pointer, gameObject) => {
         if (!this.gameOver) {
           gameObject.clearTint();
           gameObject.setScale(0.3);
           if (
             gameObject.getData("value") >
-              this.gameItemsOnScreen[0].getData("value") ||
+              this.gameItems.gameItemsOnScreen[0].getData("value") ||
             gameObject.getData("value") >
-              this.gameItemsOnScreen[1].getData("value")
+              this.gameItems.gameItemsOnScreen[1].getData("value")
           ) {
             this.score++;
             this.scoreText.setText("score: " + this.score);
-            this.gameItemsOnScreen[0].setPosition(555, 555);
-            this.gameItemsOnScreen[0] = this.gameItemsOnScreen[1];
+            this.gameItems.gameItemsOnScreen[0].setPosition(555, 555);
+            this.gameItems.gameItemsOnScreen[0] =
+              this.gameItems.gameItemsOnScreen[1];
             while (true) {
-              this.gameItemsOnScreen[1] = this.gameItems.at(
-                Phaser.Math.Between(0, this.gameItems.length - 1)
+              this.gameItems.gameItemsOnScreen[1] = this.gameItems.items.at(
+                Phaser.Math.Between(0, this.gameItems.items.length - 1)
               );
               if (
-                this.gameItemsOnScreen[0].getData("value") !==
-                this.gameItemsOnScreen[1].getData("value")
+                this.gameItems.gameItemsOnScreen[0].getData("value") !==
+                this.gameItems.gameItemsOnScreen[1].getData("value")
               ) {
                 break;
               }
             }
 
-            this.gameItemsOnScreen[0].setPosition(200, 220);
-            this.gameItemsOnScreen[1].setPosition(600, 220);
+            this.gameItems.gameItemsOnScreen[0].setPosition(200, 220);
+            this.gameItems.gameItemsOnScreen[1].setPosition(600, 220);
           } else {
             this.scoreText.setText("");
             this.promptText.setText("");
@@ -181,7 +100,7 @@ export default class MainGame extends Phaser.Scene {
       });
 
     this.input
-      .setHitArea(this.gameItemsGroup.getChildren())
+      .setHitArea(this.gameItems.getChildren())
       .on("gameobjectdown", (pointer, gameObject) => {
         if (!this.gameOver) {
           gameObject.setTint(0xdddddd);
